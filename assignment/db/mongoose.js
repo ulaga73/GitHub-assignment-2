4cb2936070b3d20702581498c7392743bd4e1223
Ulaganathan <42119967+ulaga73@users.noreply.github.com>
2023-01-13 16:04:54 +0530

    Revert "Added assignment-2"

--- assignment/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose  = require("mongoose");
-
-async function dbServer(){
-    await mongoose.connect("mongodb://localhost:27017/assignment");
-    console.log("Database is running successfully...")
-}
-
-module.exports = dbServer;
